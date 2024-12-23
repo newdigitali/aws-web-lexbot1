0e5ec865dd70fd064b6e2399118109faddcc8c5b
newdigitali <gary.schwartzberg@gmail.com>
2024-12-23 12:38:45 -0500

    Dec23a

---
+++ move_backup_files.sh
@@ -0,0 +1,5 @@
+mv /Users/garys/aws-web-lexbot1/s3-backup aws-backup/
+mv /Users/garys/aws-web-lexbot1/cloudfront-distributions.json aws-backup/
+mv /Users/garys/aws-web-lexbot1/cloudfront-distribution-config.json aws-backup/
+mv /Users/garys/aws-web-lexbot1/cognito-user-pool-config.json aws-backup/
+mv /Users/garys/aws-web-lexbot1/lex-bot-config-A1B2C3D4E5.json aws-backup/
